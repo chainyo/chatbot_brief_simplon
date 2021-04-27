6b127fa64fd02a2d4fd11ad6325537e066731499
chainyo <thomas.chaigneau@isen-ouest.yncrea.fr>
2021-04-27 12:27:45 +0200

    ajout input et bouton envoi pour fenetre de chat

--- frontend/src/components/ChatWindow.vue
+++ frontend/src/components/ChatWindow.vue
@@ -1,10 +1,31 @@
 <template>
-  
+  <div class="main-window">
+      <div id="chat-window"></div>
+      <div id="meassage-input">
+        <el-row>
+          <el-input
+            placeholder="Votre message..."
+            v-model="input"
+            clearable>
+          </el-input>
+          <el-button icon="el-icon-position" type="primary" @click="sendMessage()" circle></el-button>
+        </el-row>
+      </div>
+  </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      input: null
+    }
+  },
+  methods: {
+    sendMessage () {
 
+    }
+  }
 }
 </script>
 
--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,5 +1,15 @@
 <template>
-  
+  <el-row id="header-col">
+    <el-col :span="1" id="logo-simplon">
+      <el-avatar :size="50" :src="circleUrl"></el-avatar>
+    </el-col>
+    <el-col :span="5">
+      <el-card shadow="hover">Simplon</el-card>
+    </el-col>
+    <el-col :span="5">
+      <el-card shadow="hover">Github</el-card>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
@@ -8,6 +18,10 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+#header-col {
+    background-color: #CE0033;
+    height: 120%;
+}
 
 </style>
