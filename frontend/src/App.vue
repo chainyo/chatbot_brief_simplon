8458753f3565e5d33b9f06b66ddccc0357bc6c1a
chainyo <thomas.chaigneau@isen-ouest.yncrea.fr>
2021-04-27 11:22:10 +0200

    ajout structure router

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,24 +1,15 @@
 <template>
+  <div class="container">
+    <router-view />
+  </div>
 </template>
 
 <script>
-import ChatBot from './pages/ChatBot.vue'
-
 export default {
-  name: 'App',
-  components: {
-    ChatBot
+    name: 'App'  
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "assets/style.css"
 </style>
