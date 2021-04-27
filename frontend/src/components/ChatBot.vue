990f6f2e8fc4253fe501d6a91394b49ad02e4425
chainyo <thomas.chaigneau@isen-ouest.yncrea.fr>
2021-04-27 11:07:30 +0200

    ajout de la structure de base de l'interface

---
+++ frontend/src/components/ChatBot.vue
@@ -0,0 +1,12 @@
+<template>
+</template>
+
+<script>
+export default {
+  name: 'ChatBot'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ frontend/src/main.js
@@ -0,0 +1,12 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from '@/router'
+
+// ElementPlus components library
+import ElementPlus from 'element-plus'
+import 'element-plus/lib/theme-chalk/index.css'
+
+const chatbotApp = createApp(App)
+chatbotApp.use(router)
+chatbotApp.use(ElementPlus)
+chatbotApp.mount('#app')
