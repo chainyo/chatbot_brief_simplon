8fda15a921edda18e8c5a2e43a2b230aecf4091f
Thomas Chaigneau <50595514+ChainYo@users.noreply.github.com>
2021-04-28 12:09:34 +0200

    Update query.py

--- app/server/query.py
+++ app/server/query.py
@@ -9,11 +9,11 @@ class DB :
     @classmethod
     def connexion(cls) :
         cls.client = motor.motor_asyncio.AsyncIOMotorClient("mongodb+srv://TJLL:M3G4H377ABot@chatbotcluster.zblke.mongodb.net/?retryWrites=true&w=majority")
-        cls.db = cls.client['chatDB']
+        cls.db = cls.client['ChatDB']
 
 
     @classmethod
-    async def find_one(cls, tag):
+    async def find_answer(cls, tag):
         cls.connexion()
-        data = await cls.db.intents.find_one({"tag": tag})
+        data = await cls.db.intents.find_one({"tag": tag}, {'_id': 0)
         return data
