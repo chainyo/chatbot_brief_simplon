e26520a0e7a5d10440fe1a22fbec16199bab6af3
Jamal_Ibanni <60659076+sialorama@users.noreply.github.com>
2021-04-29 14:08:11 +0200

    Update main.py

--- API_on_ Docker/main.py
+++ API_on_ Docker/main.py
@@ -3,10 +3,6 @@ from fastapi import FastAPI
 from starlette.responses import RedirectResponse
 from query import DB
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
-
-
 app = FastAPI()
 
 @app.get("/", tags=["Root"])
