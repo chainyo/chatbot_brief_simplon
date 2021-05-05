231091d9e27b93cabf02d384058f28c7a5e74fdc
LueMar-R <ludivine.maintier@isen-ouest.yncrea.fr>
2021-05-05 16:40:50 +0200

    model essai

--- l_training.py
+++ l_training.py
@@ -1,6 +1,6 @@
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Dense,Activation,Dropout
+from keras.layers import Dense,Dropout
 from keras.optimizers import SGD
 import random
 import pickle
@@ -28,13 +28,16 @@ def create_model(data_path):
     print(model.summary())
 
     hist = model.fit(np.array(X_train), np.array(y_train), epochs=500, batch_size=5, verbose=1)
-    model.save('l_test/chatbot_model.h5', hist)
+    model.save('l_test/chatbot_modelX.h5', hist)
     try:
         tfjs.converters.save_keras_model(model, "l_test/tfjsmodel")
         print("model crée")
-        pickle.dump("l_test/bow", bag_of_words)
+        print(bag_of_words)
+        pickle.dump(bag_of_words, open("l_test/bow.pkl", "wb"))
     except:
         print("pb_enregistrement")
 
 data_path = './model/ressources/intents/intents.json'
-create_model(data_path)
+modelX = create_model(data_path)
+
+print(modelX.summary)
